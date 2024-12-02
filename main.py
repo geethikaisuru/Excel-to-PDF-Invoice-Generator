9f242a17b37baf45142c0ce16a1641147a1f4487
Geethika Isuru Sampath <35458073+geethikaisuru@users.noreply.github.com>
2024-12-02 22:55:32 +0530

    added xcel to df

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import pandas as pd
+import glob
+
+
+filepaths = glob.glob("Excel-Data/*.xlsx")
+
+# Load the data
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
